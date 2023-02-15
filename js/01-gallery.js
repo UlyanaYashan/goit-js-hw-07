8adf265057524f42d459a03bae6226dfc5649ca7
UlyanaYashan <ulyanalyala@gmail.com>
2023-02-15 11:55:05 +0200

    початок роботи

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const mainDivEl = document.querySelector('.gallery');
+
+const listMarkupImage = creatListImage(galleryItems);
+
+
+mainDivEl.insertAdjacentHTML('beforeend', listMarkupImage) ;
+mainDivEl.addEventListener('click', onMainDivEl);
+
+function creatListImage (galleryItems) {
+    
+return galleryItems.map(({preview, original, description}) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}"><img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      /></a>
+  </div>`
+}).join('');
+
+}
+
+function onMainDivEl(evt) {
+evt.preventDefault();
+const actualeImageUrl = evt.target.dataset.source;
+console.log(actualeImageUrl);
+const isImage = evt.target.classList.contains("gallery__image");
+if (!isImage) {
+  return;
+}
+
+
+const instance = basicLightbox.create(`
+    <img src="${actualeImageUrl}" width="800" height="600">
+`)
+
+instance.show()
+
+
+mainDivEl.addEventListener('keydown', (evt) => {
+  if (evt.code === 'Escape') {
+    instance.close();
+  }
+})
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,41 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const mainDivEl = document.querySelector('.gallery');
+
+const listMarkupImage = creatListImage(galleryItems);
+
+
+mainDivEl.insertAdjacentHTML('beforeend', listMarkupImage) ;
+mainDivEl.addEventListener('click', onMainDivEl);
+
+function creatListImage (galleryItems) {
+    
+return galleryItems.map(({preview, original, description}) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}"><img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      /></a>
+  </div>`
+}).join('');
+}
+
+function onMainDivEl(evt) {
+    evt.preventDefault();
+    const actualeImageUrl = evt.target.dataset.source;
+    console.log(actualeImageUrl);
+    const isImage = evt.target.classList.contains("gallery__image");
+    if (!isImage) {
+      return;
+    }
+
+
+}
+// const lightbox = new SimpleLightbox('.gallery a', {
+//     captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+//   });
